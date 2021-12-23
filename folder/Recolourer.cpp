2edb104be8cb7619c5f8b38b9ef5969387be0235
Adham-wali <96571274+Adham-wali@users.noreply.github.com>
2021-12-23 18:54:05 +0300

    Create Recolourer.cpp

---
+++ folder/Recolourer.cpp
@@ -0,0 +1,15 @@
+#include "BmpStructures.h"
+#include "ImgFunctions.h"
+#include <iostream>
+int main() {
+    RgbImg img = readRgbImg("totaly_normal_rainbow.bmp");
+    for (size_t row = 0; row < img.width; ++row)
+        for (size_t col = 0; col < img.height; ++col){
+            BYTE tmp = img.pixels[col][row].Blue;
+            img.pixels[col][row].Blue = img.pixels[col][row].Red;
+            img.pixels[col][row].Red = tmp;
+        }
+    writeRgbImg("recoloured_rainbow.bmp", img);
+    deleteRgbImg(img);
+    return 0;
+}
